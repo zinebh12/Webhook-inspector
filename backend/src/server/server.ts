e229358f4e81582f257553d6c3398e85324538eb
Zineb <krehtyazeh@gmail.com>
2026-09-15 22:34:43 +0100

    Connect prisma to express

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,12 +1,20 @@
 import express, { type Express, type Request, type Response } from "express";
 import dotenv from "dotenv";
+import { db } from "../prisma/db";
 
 dotenv.config();
 const app: Express = express();
 const port = 3000;
 
+//health check!
 app.get("/api/health", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+// get users
+app.get("/api/user", (req: Request, res: Response) => {
+  const result = db.sql.public.user.select("id", "email").build();
+  res.json(result);
 });
 
 app.listen(port, () => {
