4e17243a408b56c70b428822f42a02dad5f4b036
Zineb <krehtyazeh@gmail.com>
2026-09-15 21:54:27 +0100

    resetting prisma + schemas
    bug fix

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,12 +1,13 @@
 import "dotenv/config";
+
 import { definePrismaConfig } from "prisma/config";
 import { defineConfig as ormConfig } from "@prisma/orm-postgres/config";
 
 export default definePrismaConfig({
   orm: ormConfig({
-    contract: "./prisma/contract.prisma",
-    db: {
-      connection: process.env["DATABASE_URL"]!,
-    },
+    contract: "./src/prisma/contract.prisma",
+    // db: {
+    //   connection: process.env["DATABASE_URL"]!,
+    // },
   }),
 });
