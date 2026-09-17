dbc3e5b61d5e65e3c53d1e5ae185ce326aa6a648
Zineb <krehtyazeh@gmail.com>
2026-09-17 17:07:45 +0100

    Protected routes

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { registerAuth, loginAuth } from '../controllers/auth';
-
+import { registerAuth, loginAuth, getCurrentUser, logout } from '../controllers/auth';
+import { authMiddleware } from '../middleware/authMiddleware';
 const router = Router();
 
 router.post('/register', registerAuth);
 router.post('/login', loginAuth);
+router.post('/logout', logout);
+router.post('/user', authMiddleware, getCurrentUser);
 
 export default router;
