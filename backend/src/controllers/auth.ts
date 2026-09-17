b6c76050838d54b613a548a043ac115257abbfb2
Zineb <krehtyazeh@gmail.com>
2026-09-17 16:10:11 +0100

    JWT AUTH + gitignore update

---
+++ backend/src/controllers/auth.ts
@@ -0,0 +1,82 @@
+import { registerSchemma, loginSchema } from '../schemas/authSchema';
+import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
+import type { Response } from 'express';
+import { db } from '../prisma/db';
+import type { authRequest } from '../types/auth';
+export const registerAuth = async (req: authRequest, res: Response) => {
+  try {
+    const validatedData = registerSchemma.safeParse(req.body);
+    if (!validatedData.success) {
+      return res.status(400).json({
+        error: 'Validation failed',
+        details: validatedData.error.issues,
+      });
+    }
+    const { email, password } = validatedData.data;
+    const existingUser = await db.orm.public.User.where({ email }).first();
+    if (existingUser?.email) {
+      return res.status(400).json({
+        error: 'User already exsits',
+      });
+    }
+    const passwordHash = await argon2.hash(password);
+    await db.orm.public.User.create({
+      email,
+      password: passwordHash,
+    });
+    return res.status(201).json({
+      message: 'User registered successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: 'Failed to register user',
+    });
+  }
+};
+
+export const loginAuth = async (req: authRequest, res: Response) => {
+  const JWT_SECRET = process.env.JWT_SECRET;
+  if (!JWT_SECRET) {
+    throw new Error('JWT_SECRET is not set');
+  }
+  try {
+    const validatedData = loginSchema.safeParse(req.body);
+    if (!validatedData.success) {
+      return res.status(400).json({
+        error: 'Validation failed',
+        details: validatedData.error.issues,
+      });
+    }
+    const { email, password } = validatedData.data;
+    const user = await db.orm.public.User.where({ email }).first();
+    if (!user ) {
+      return res.status(401).json({
+        error: 'Invalid email or password',
+      });
+    }
+    const passwordMatches = await argon2.verify(password, user?.password);
+    if (!passwordMatches) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({
+      message: 'Login successful!',
+      user: { id: user.id, email: user.email },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to login' });
+  }
+};
