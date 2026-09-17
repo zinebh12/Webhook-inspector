844831deff32462c42d389f0c6bedb18192b5cd3
Zineb <krehtyazeh@gmail.com>
2026-09-17 18:29:18 +0100

    server.ts cleanup

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,6 +1,5 @@
 import express, { type Express, type Request, type Response } from 'express';
 import dotenv from 'dotenv';
-import { db } from '../prisma/db';
 import authRoute from './../routes/authRoute';
 
 dotenv.config();
@@ -11,12 +10,6 @@ app.use('/api/auth', authRoute);
 //health check!
 app.get('/api/health', (req: Request, res: Response) => {
   res.send('Hello World!');
-});
-
-// get users
-app.get('/api/user', (req: Request, res: Response) => {
-  const result = db.sql.public.user.select('id', 'email').build();
-  res.json(result);
 });
 
 app.listen(port, () => {
