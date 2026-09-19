97bce7ec3e17a754fb1858b643bfbacaf95121b1
Zineb <krehtyazeh@gmail.com>
2026-09-19 23:10:20 +0100

    clear commit history

--- backend/src/controllers/webhookRequests.ts
+++ backend/src/controllers/webhookRequests.ts
@@ -124,14 +124,48 @@ export const deleteRequest = async (req: authRequest, res: Response) => {
     if (!webhookRequest || webhookRequest.endpoint.userId !== req.userId) {
       return res.status(404).json({ error: 'Request not found.' });
     }
-    
+
     await db.orm.public.WebhookRequest.where({ id }).delete();
 
-    return res.status(204).send();
+    return res.status(200).json({ message: 'Successfully deleted request' });
   } catch (error) {
     console.error(error);
     res.status(500).json({
       error: 'Failed to delete Request',
+    });
+  }
+};
+
+export const clearRequests = async (req: authRequest, res: Response) => {
+  try {
+    const id = req.params.id;
+    const userId = req.userId;
+
+    if (!id || typeof id !== 'string') {
+      return res.status(400).json({
+        error: 'Invalid endpoint ID',
+      });
+    }
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Not authenticated' });
+    }
+
+    const endpoint = await db.orm.public.WebhookEndpoint.where({ id, userId }).first();
+
+    if (!endpoint) {
+      return res.status(404).json({ error: 'No endpoint found.' });
+    }
+
+    const deletedRequests = await db.orm.public.WebhookRequest.where({
+      endpointId: endpoint.id,
+    }).deleteAndCount();
+
+    return res.status(200).json({ message: `cleared ${deletedRequests} requests` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: 'Failed to delete Requests',
     });
   }
 };
