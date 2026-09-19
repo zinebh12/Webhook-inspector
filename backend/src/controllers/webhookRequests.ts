25eb93ad9000b3cac7c3935e68f9356e4167cb9b
Zineb <krehtyazeh@gmail.com>
2026-09-19 20:45:20 +0100

    save incoming requests to db + tested with postman

--- backend/src/controllers/webhookRequests.ts
+++ backend/src/controllers/webhookRequests.ts
@@ -3,30 +3,32 @@ import type { Request, Response } from 'express';
 import { db } from '../prisma/db';
 
 export const createRequest = async (req: Request, res: Response) => {
-  try {
-    const requests = {
-      method: req.method,
-      headers: req.headers,
-      body: req.body,
-      query: req.query,
-      ip: req.ip,
-      statusCode: 200,
-    };
-    //handle inactive / invalide endpoints
+  try {    
     const slug = req.params.slug;
+    
     if (!slug || typeof slug !== 'string') {
       return res.status(400).json({
         error: 'Invalid endpoint ID',
       });
     }
     const endpoint = await db.orm.public.WebhookEndpoint.where({ url: slug }).first();
-    const inactiveEndpoint = await db.orm.public.WebhookEndpoint.where({ isActive: false }).first();
+
     if (!endpoint) {
       return res.status(404).json({ error: 'No endpoint found.' });
     }
-    if (inactiveEndpoint) {
+    if (!endpoint.isActive) {
       return res.status(401).json({ error: 'Endpoint is not active.' });
     }
+
+    await db.orm.public.WebhookRequest.create({
+      method: req.method,
+      headers: JSON.parse(JSON.stringify(req.headers)),
+      body: req.body ?? null,
+      query: JSON.parse(JSON.stringify(req.query)),
+      ip: req.ip,
+      statusCode: 200,
+      endpointId: endpoint.id,
+    });
 
     return res.status(200).json({ received: true });
   } catch (error) {
