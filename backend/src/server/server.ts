6f5de3e861e2f04194e788595b49edd8998f20d2
Zineb <krehtyazeh@gmail.com>
2026-09-22 11:37:50 +0000

    set up socket.io

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,6 +1,8 @@
 import express, { type Express, type Request, type Response } from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 import authRoute from './../routes/authRoute';
 import endpointRoutes from './../routes/endpointRoutes';
 import webhookRequestsRoutes from './../routes/webhookRequestsRoutes';
@@ -9,9 +11,11 @@ import webhookReceiverRoute from '../routes/webhookReceiverRoute';
 dotenv.config();
 const app: Express = express();
 const port = 3000;
-
+const server = createServer(app);
+const io = new Server(server);
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/api/auth', authRoute);
 app.use('/api/webhook/endpoints', endpointRoutes);
 app.use('/api/webhook/request', webhookRequestsRoutes);
@@ -22,6 +26,11 @@ app.get('/api/health', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+// socket io check!
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
